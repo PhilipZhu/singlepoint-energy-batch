PhilipZhu/singlepoint-energy-batch

commit f3667b64715722f611445c23c5bec5921444eb07

updated examples and templates to use prepare_input() in place of split_input()

template_software_ini/by_software/molpro/prepare_input.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# split_input
+# prepare_input
 #   Input  : <xyz file> <HEAD file> <TAIL file> <charge> <spin=2*S> <frozen> <dummy>
-#   Output : Generates input files for ${CALC_EXE} with name format xxNNNN. Each
-#            file will be renamed as ${CALC_INP_FILENAME}. Each job will run as:
+#   Output : The standard output generated by this script will be written to a
+#            file named ${CALC_INP_FILENAME}. Each job will run as:
 #
 #            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
 
@@ -18,4 +18,7 @@ dummy="$7"
 SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXXX/'"${chg}"'/g' | sed 's/SSSSSS/'"${spin}"'/g' | sed 's/ZZZZZZ/'"${frozen}"'/g' | sed 's/DDDummyDDD/'"${dummy}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
 STAIL="$(cat "${TAIL_FN}" | sed 's/XXXXXX/'"${chg}"'/g' | sed 's/SSSSSS/'"${spin}"'/g' | sed 's/ZZZZZZ/'"${frozen}"'/g' | sed 's/DDDummyDDD/'"${dummy}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
 CHARCOMMENT='!'
-csplit -z -b "%05d" <(sed 's@[[:space:]]*: @1 @g' "${input_xyz_file}" | sed '\@^[[:space:]]*[0-9]*[[:space:]]*$@{N;s@^.*\n@'"${STAIL}"'\n'"${CHARCOMMENT}"'###CSPLIT####\n'"${SHEAD}"'\n'"${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT}"' COMMENT FROM XYZ FILE : @g}' | sed -n '\@^'"${CHARCOMMENT}"'###CSPLIT####@,$p' && echo -e "${STAIL}") '/^'"${CHARCOMMENT}"'###CSPLIT####$/' '{*}'
+echo -e "${SHEAD}"
+echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
+tail -n +2 "${input_xyz_file}"
+echo -e "${STAIL}"

examples/1-example_singlepoint/g09.ini/prepare_input.sh
@@ -1,0 +1,23 @@
+#!/bin/bash
+
+# prepare_input
+#   Input  : <xyz file> <charge> <multiplicity> <HEAD file> <TAIL file>
+#   Output : The standard output generated by this script will be written to a
+#            file named ${CALC_INP_FILENAME}. Each job will run as:
+#
+#            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
+
+input_xyz_file="$1"
+chg="$2"
+mult="$3"
+HEAD_FN="$4"
+TAIL_FN="$5"
+
+chgmult="${chg} ${mult}"
+SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXX/'"${chgmult}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+STAIL="$(cat "${TAIL_FN}"                                  | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+CHARCOMMENT='!'
+echo -e "${SHEAD}"
+echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
+tail -n +2 "${input_xyz_file}"
+echo -e "${STAIL}"

examples/2-example_mbd/g09.ini/prepare_input.sh
@@ -1,0 +1,23 @@
+#!/bin/bash
+
+# prepare_input
+#   Input  : <xyz file> <charge> <multiplicity> <HEAD file> <TAIL file>
+#   Output : The standard output generated by this script will be written to a
+#            file named ${CALC_INP_FILENAME}. Each job will run as:
+#
+#            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
+
+input_xyz_file="$1"
+chg="$2"
+mult="$3"
+HEAD_FN="$4"
+TAIL_FN="$5"
+
+chgmult="${chg} ${mult}"
+SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXX/'"${chgmult}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+STAIL="$(cat "${TAIL_FN}"                                  | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+CHARCOMMENT='!'
+echo -e "${SHEAD}"
+echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
+tail -n +2 "${input_xyz_file}"
+echo -e "${STAIL}"

template_software_ini/by_software/g09/prepare_input.sh
@@ -1,0 +1,23 @@
+#!/bin/bash
+
+# prepare_input
+#   Input  : <xyz file> <charge> <multiplicity> <HEAD file> <TAIL file>
+#   Output : The standard output generated by this script will be written to a
+#            file named ${CALC_INP_FILENAME}. Each job will run as:
+#
+#            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
+
+input_xyz_file="$1"
+chg="$2"
+mult="$3"
+HEAD_FN="$4"
+TAIL_FN="$5"
+
+chgmult="${chg} ${mult}"
+SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXX/'"${chgmult}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+STAIL="$(cat "${TAIL_FN}"                                  | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+CHARCOMMENT='!'
+echo -e "${SHEAD}"
+echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
+tail -n +2 "${input_xyz_file}"
+echo -e "${STAIL}"

template_software_ini/by_software/orca/prepare_input.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# split_input
+# prepare_input
 #   Input  : <xyz file> <charge> <multiplicity> <HEAD file> <TAIL file>
-#   Output : Generates input files for ${CALC_EXE} with name format xxNNNN. Each
-#            file will be renamed as ${CALC_INP_FILENAME}. Each job will run as:
+#   Output : The standard output generated by this script will be written to a
+#            file named ${CALC_INP_FILENAME}. Each job will run as:
 #
 #            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
 
@@ -18,4 +18,7 @@ chgmult="${chg} ${mult}"
 SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXXX/'"${chgmult}"'/g' | sed 's/YYYYYY/'"${NPROC}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
 STAIL="$(cat "${TAIL_FN}"                                                                   | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
 CHARCOMMENT='#'
-csplit -z -b "%05d" <(sed '\@^[[:space:]]*[0-9]*[[:space:]]*$@{N;s@^.*\n@'"${STAIL}"'\n'"${CHARCOMMENT}"'###CSPLIT####\n'"${SHEAD}"'\n'"${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT}"' COMMENT FROM XYZ FILE : @g}' ${input_xyz_file} | sed -n '\@^'"${CHARCOMMENT}"'###CSPLIT####@,$p' && echo -e "${STAIL}") '/^'"${CHARCOMMENT}"'###CSPLIT####$/' '{*}'
+echo -e "${SHEAD}"
+echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
+tail -n +2 "${input_xyz_file}"
+echo -e "${STAIL}"

template_software_ini/by_software/qchem/prepare_input.sh
@@ -1,0 +1,23 @@
+#!/bin/bash
+
+# prepare_input
+#   Input  : <xyz file> <charge> <multiplicity> <HEAD file> <TAIL file>
+#   Output : The standard output generated by this script will be written to a
+#            file named ${CALC_INP_FILENAME}. Each job will run as:
+#
+#            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
+
+input_xyz_file="$1"
+chg="$2"
+mult="$3"
+HEAD_FN="$4"
+TAIL_FN="$5"
+
+chgmult="${chg} ${mult}"
+SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXX/'"${chgmult}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+STAIL="$(cat "${TAIL_FN}"                                  | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
+CHARCOMMENT='!'
+echo -ne "${SHEAD}"
+echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
+tail -n +2 "${input_xyz_file}"
+echo -e "${STAIL}"

examples/1-example_singlepoint/g09.ini/software.ini
@@ -24,6 +24,7 @@ TAIL_TEMPLATE="input_template/TAIL"
 #-------------------------------#
 
 # required definitions
+export INP_XYZ_FILENAME="input.xyz"
 export CALC_EXE="g09"
 export CALC_INP_FILENAME="config.com"
 export CALC_STDOUT_FILENAME="config.stdout"
@@ -32,18 +33,18 @@ export CALC_STDOUT_FILENAME="config.stdout"
 # REQUIRED function definitions
 #-------------------------------#
 
-# split_input
-#   Input  : <xyz file>
-#   Output : Generates input files for ${CALC_EXE} with name format xxNNNN. Each
-#            file will be renamed as ${CALC_INP_FILENAME}. Each job will run as:
+# prepare_input
+#   Invoked in each job folder.
+#   Output : Generates ${CALC_INP_FILENAME} as input for ${CALC_EXE} from a
+#            single-frame XYZ file ${INP_XYZ_FILENAME}. Each job will run as:
 #
 #            ${CALC_EXE} "${CALC_INP_FILENAME}" > "${CALC_STDOUT_FILENAME}"
 #
-split_input() {
-  local input_xyz_file="$1"
+prepare_input() {
+  local input_xyz_file="${INP_XYZ_FILENAME}"
   local HEAD_FN="${SOFTWARE_INI_DIR}/${HEAD_TEMPLATE}"
   local TAIL_FN="${SOFTWARE_INI_DIR}/${TAIL_TEMPLATE}"
-  bash "${SOFTWARE_INI_DIR}/split_input.sh" "${input_xyz_file}" "${chg}" "${mult}" "${HEAD_FN}" "${TAIL_FN}"
+  bash "${SOFTWARE_INI_DIR}/prepare_input.sh" "${input_xyz_file}" "${chg}" "${mult}" "${HEAD_FN}" "${TAIL_FN}" > "${CALC_INP_FILENAME}"
 }
 
 # isfinished

examples/1-example_singlepoint/g09.ini/split_input.sh
@@ -1,20 +1,0 @@
-#!/bin/bash
-
-# split_input
-#   Input  : <xyz file> <charge> <multiplicity> <HEAD file> <TAIL file>
-#   Output : Generates input files for ${CALC_EXE} with name format xxNNNN. Each
-#            file will be renamed as ${CALC_INP_FILENAME}. Each job will run as:
-#
-#            ${CALC_EXE} ${CALC_INP_FILENAME} > ${CALC_STDOUT_FILENAME}
-
-input_xyz_file="$1"
-chg="$2"
-mult="$3"
-HEAD_FN="$4"
-TAIL_FN="$5"
-
-chgmult="${chg} ${mult}"
-SHEAD="$(cat "${HEAD_FN}" | sed 's/XXXXX/'"${chgmult}"'/g' | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
-STAIL="$(cat "${TAIL_FN}"                                  | awk -v ORS='\\n' '{gsub(/\r$/,"")}1' | sed 's/\\n$//g')"
-CHARCOMMENT='!'
-csplit -z -b "%05d" <(sed '\@^[[:space:]]*[0-9]*[[:space:]]*$@{N;s@^.*\n@'"${STAIL}"'\n'"${CHARCOMMENT}"'###CSPLIT####\n'"${SHEAD}"'\n'"${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT}"' COMMENT FROM XYZ FILE : @g}' ${input_xyz_file} | sed -n '\@^'"${CHARCOMMENT}"'###CSPLIT####@,$p' && echo -e "${STAIL}") '/^'"${CHARCOMMENT}"'###CSPLIT####$/' '{*}'

commit 65289a44196861e539848479a39e18921a0e8aac

BUGFIX: template_software_ini/molpro before v2.0.0 supported xyz file with dummy atoms ("X :") generated by mbd.sh; broken by commit f3667b6 and now fixed

template_software_ini/by_software/molpro/prepare_input.sh
@@ -20,5 +20,5 @@ STAIL="$(cat "${TAIL_FN}" | sed 's/XXXXXX/'"${chg}"'/g' | sed 's/SSSSSS/'"${spin
 CHARCOMMENT='!'
 echo -e "${SHEAD}"
 echo -n "${CHARCOMMENT}${CHARCOMMENT}${CHARCOMMENT} COMMENT FROM XYZ FILE : "
-tail -n +2 "${input_xyz_file}"
+tail -n +2 "${input_xyz_file}" | sed 's@[[:space:]]*: @1 @g'
 echo -e "${STAIL}"